b462126d71f847dba3557872fd97ddda801da90a
MohammedFaizan10 <149553652+MohammedFaizan10@users.noreply.github.com>
2025-08-06 23:46:02 +0530

    java_patterns.java

--- java_patterns.java
+++ java_patterns.java
@@ -32,7 +32,7 @@ public class java_patterns {
         // }
 
 
-        // HALF-PYRAMID PATTERN
+        // PATTERN 3 : HALF-PYRAMID PATTERN
 
         // System.out.println("ENTER YOUR NUMBER : ");
         // int numb = sc.nextInt();
@@ -59,5 +59,6 @@ public class java_patterns {
         }
     }
 }
+
 
 
