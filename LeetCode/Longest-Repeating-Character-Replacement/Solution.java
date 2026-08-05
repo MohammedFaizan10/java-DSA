7aa9e6c890452e505c7fd8b6a2e62a7ca0bddb39
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-08-05 14:42:22 +0530

    Add solution for Longest Repeating Character Replacement

--- LeetCode/Longest-Repeating-Character-Replacement/Solution.java
+++ LeetCode/Longest-Repeating-Character-Replacement/Solution.java
@@ -10,26 +10,27 @@
 10        for(int high = 0 ; high < n ; high++){
 11            arr[s.charAt(high) - 'A']++;
 12            len = high - low +1;
-13            for(int i = 0 ; i < 26 ; i++){
-14                maxfreq = Math.max(maxfreq,arr[i]);
-15            }
-16            diff = len - maxfreq;
-17            while(diff > k){
-18                arr[s.charAt(low)-'A']--;
-19                low++;
-20                len = high - low +1;
-21                maxfreq = 0;
-22                for(int i = 0 ; i < 26 ; i++){
-23                    maxfreq = Math.max(maxfreq,arr[i]);
-24                }
-25                diff = len - maxfreq; 
-26            }
-27            if(diff <= k){
-28                maxlen = Math.max(len,maxlen);
-29            }
-30
+13            maxfreq = 0;
+14            for(int i = 0 ; i < 26 ; i++){
+15                maxfreq = Math.max(maxfreq,arr[i]);
+16            }
+17            diff = len - maxfreq;
+18            while(diff > k){
+19                arr[s.charAt(low)-'A']--;
+20                low++;
+21                len = high - low +1;
+22                maxfreq = 0;
+23                for(int i = 0 ; i < 26 ; i++){
+24                    maxfreq = Math.max(maxfreq,arr[i]);
+25                }
+26                diff = len - maxfreq; 
+27            }
+28            if(diff <= k){
+29                maxlen = Math.max(len,maxlen);
+30            }
 31
-32        }
-33        return maxlen;
-34    }
-35}
+32
+33        }
+34        return maxlen;
+35    }
+36}
