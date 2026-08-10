4f8934b0fb31d250818e0c2dfa44b6865d6758b6
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-08-10 10:09:53 +0530

    Add solution for Subarrays with K Different Integers

---
+++ LeetCode/Subarrays-with-K-Different-Integers/Solution.java
@@ -0,0 +1,27 @@
+1class Solution {
+2    public int subarraysWithKDistinct(int[] nums, int k) {
+3        return atMostK(k,nums) - atMostK(k-1,nums);
+4    }
+5    public int atMostK(int k , int nums[]){
+6        int n = nums.length;
+7        int low = 0;
+8        HashMap<Integer,Integer> map = new HashMap<>();
+9        int count = 0;
+10        for(int high = 0 ; high < n ; high++){
+11            int num = nums[high];
+12            map.put(num, map.getOrDefault(num,0)+1);
+13            while(map.size() > k){
+14                int numL = nums[low];
+15                map.put(numL,map.getOrDefault(numL,0)-1);
+16                if(map.get(numL) == 0){
+17                    map.remove(numL);
+18                }
+19                
+20                low++;
+21                
+22            }
+23            count += high - low +1;
+24        }
+25        return count;
+26    }
+27}
