43b56c49563184baeea6d99a18241df228df8e94
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-08-14 00:04:08 +0530

    Add solution for Maximum Subarray Sum with One Deletion

--- LeetCode/Maximum-Subarray-Sum-with-One-Deletion/Solution.java
+++ LeetCode/Maximum-Subarray-Sum-with-One-Deletion/Solution.java
@@ -1,30 +1,16 @@
 1class Solution {
 2    public int maximumSum(int[] arr) {
-3        int n = arr.length;
-4        int noDelete = 0;                          // BESTENDING
-5        int oneDelete = Integer.MIN_VALUE;         // BESTENDING WITH ONE DELETION
-6        int res = Integer.MIN_VALUE;
-7
-8        for(int i = 0 ; i < n; i++){
-9            int prevNoDelete = noDelete;
-10            int prevOneDelete = oneDelete;
+3       int nodelete = arr[0];
+4        int ans = arr[0];
+5        int onedelete = arr[0];
+6        for(int i = 1;i<arr.length;i++){
+7            onedelete = Math.max(nodelete,onedelete+arr[i]);
+8            nodelete = Math.max(nodelete+arr[i],arr[i]);
+9
+10            ans  = Math.max(ans,Math.max(nodelete,onedelete));
 11
-12            noDelete = Math.max(arr[i], prevNoDelete + arr[i]);
-13            if(i == 0){
-14                oneDelete = Integer.MIN_VALUE;
-15            }
-16            else{
-17                int v1 = Integer.MIN_VALUE;
-18                if(prevOneDelete != Integer.MIN_VALUE){
-19                    v1 = prevOneDelete + arr[i];
-20                }
-21                oneDelete = Math.max( v1, prevNoDelete);
-22            }
-23            
-24            res = Math.max(res,Math.max(oneDelete,noDelete));
-25        }
-26
-27        return res;
-28
-29    }
-30}
+12        }
+13        return ans;
+14
+15    }
+16}
