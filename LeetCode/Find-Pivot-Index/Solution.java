2b7eef1f83b75bb24415ba0857078f8d80f6b61d
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-08-18 00:38:04 +0530

    Add solution for Find Pivot Index

---
+++ LeetCode/Find-Pivot-Index/Solution.java
@@ -0,0 +1,30 @@
+1class Solution {
+2    public int pivotIndex(int[] nums) {
+3        int n = nums.length;
+4        int left = 0;
+5        int right = 0;
+6        
+7        int sum = 0;
+8
+9
+10        for(int i = 0 ; i < n ; i++){
+11            sum += nums[i];
+12        }
+13
+14        if(sum == nums[0]){
+15            return 0;
+16        }
+17        
+18
+19        for(int i = 1 ; i < n ; i++){
+20            left += nums[i-1];
+21            right = sum - nums[i] - left;
+22
+23            if(left == right ){
+24                return i;
+25            }
+26        }
+27
+28        return -1;
+29    }
+30}
