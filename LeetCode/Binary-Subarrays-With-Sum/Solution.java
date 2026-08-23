b6d21db2e29326af4b9810d1ec252c943c1a65e4
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-08-23 21:18:23 +0530

    Add solution for Binary Subarrays With Sum

--- LeetCode/Binary-Subarrays-With-Sum/Solution.java
+++ LeetCode/Binary-Subarrays-With-Sum/Solution.java
@@ -1,31 +1,40 @@
-1class Solution {
-2    public int numSubarraysWithSum(int[] nums, int goal) {
-3        return atMostK(nums,goal) - atMostK(nums,goal-1);
-4    }
-5    public int atMostK(int nums[] , int goal){
-6        if(goal < 0){
-7            return 0;
-8        }
-9        int n = nums.length;
-10        int low = 0;
-11        int count = 0;
-12        int max = Integer.MIN_VALUE;
-13        int sum = 0;
-14        for(int high = 0 ; high < n;  high++){
-15            sum += nums[high];
-16            
-17            while(sum > goal){
-18                if(low == n){
-19                    break;
-20                }
-21                sum -= nums[low];
-22                
-23                low++;
-24            }
-25            if(sum <= goal){
-26                count += high - low +1 ;
-27            }
-28        }
-29        return count;
-30    }
-31}
+[0, 0, 0, 0, 0]
+0
+[1, 1, 1, 1, 1]
+3
+[0, 1, 0, 1, 0]
+2
+[1, 0, 1, 0, 1]
+3
+[1, 0, 0, 1]
+2
+[0, 1, 0, 0]
+1
+[1, 1, 1, 0]
+3
+[0, 0, 0]
+0
+[1, 1, 0]
+2
+[1, 1, 0, 0, 0, 1, 1]
+4
+[0, 1, 1, 0, 1, 0, 0]
+3
+[1, 1, 1, 1, 1, 1, 1]
+4
+[0, 0, 0, 1, 1, 1]
+3
+[1, 0, 1, 0]
+2
+[0, 0, 1, 0, 0]
+1
+[1, 1, 0, 1, 0, 1, 0]
+4
+[1, 1, 1, 0, 0, 1, 1, 0]
+5
+[0, 1, 0, 1, 0, 1, 0, 1, 0]
+4
+[1, 0, 1, 1, 0, 1, 1, 0, 0]
+5
+[0, 1, 1, 0, 0, 1, 0, 1, 1]
+5
