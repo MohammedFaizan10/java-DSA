c086b390a0f37f46de01313ff27d8e49c2fac9bf
Mohammed Faizan Ahmed <149553652+MohammedFaizan10@users.noreply.github.com>
2026-09-07 00:20:37 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.java
+++ LeetCode/Find-the-Duplicate-Number/Solution.java
@@ -9,18 +9,20 @@
 9            fast = nums[fast];
 10            fast = nums[fast];
 11            if(slow == fast){
-12                slow = 0;
-13                while(slow != fast){
-14                    slow = nums[slow];
-15                    fast = nums[fast];
-16                }
-17                iscycle = false;
-18                return slow;
-19
-20            }
-21        }
-22
-23        return -1;
-24        
-25    }
-26}
+12                iscycle = false;
+13                slow = 0;
+14                
+15                while(slow != fast){
+16                    slow = nums[slow];
+17                    fast = nums[fast];
+18                }
+19                
+20                return slow;
+21
+22            }
+23        }
+24
+25        return -1;
+26        
+27    }
+28}
